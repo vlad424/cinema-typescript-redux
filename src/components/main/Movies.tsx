4dbcf211bc532d05011f62937c3a8bf56ff5e409
vlad424 <vlad6754687@gmail.com>
2022-03-29 15:17:28 +0700

    feat: work on design

--- src/components/main/Movies.tsx
+++ src/components/main/Movies.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import './movies.css'
+
 const Movies = () => {
     const movie = {
         'popular': [
             {content: ".../", img: "null", id: 21},
             {content: ".../", img: "null", id: 19},
+            {content: ".../", img: "null", id: 9},
+            {content: ".../", img: "null", id: 9},
             {content: ".../", img: "null", id: 9},
             {content: ".../", img: "null", id: 1}
         ],
@@ -19,12 +23,20 @@ const Movies = () => {
     return (
         <div className='main_wrap'>
             <div className='popular'>
-                {movie.popular.map(mov => {
-                return (
-                    <div className="movie">
-                        {mov.id}
-                    </div>
-                );})}                    
+                <span className="main_h">
+                    <p>POPULAR</p>
+                    <p className="hidden_h">
+                        {'#'}
+                    </p>
+                </span>
+                <div className="mov_popular">
+                    {movie.popular.map(mov => {
+                    return (
+                        <div className="movie">
+                            {mov.id}
+                        </div>
+                    );})}
+                </div>
             </div>
 
         </div>
